44eb7f56e4085c666008621f469efde1298f7ec4
Kresna Sucandra <kresnasucandra@gmail.com>
2022-10-31 10:34:51 +0800

    data types

--- msg.py
+++ msg.py
@@ -1,2 +1,20 @@
+# data types
+int = 0
+float = 1.0
+str = "Hello, string!"
+bool = False
+None
+
+# print()
 msg = "Hello, world!"
 print(msg)
+print(int, float, str, bool, None)
+
+# type()
+print(type(int))
+print(type(float))
+print(type(str))
+print(type(bool))
+print(type(None))
+
+print(type("something"))
