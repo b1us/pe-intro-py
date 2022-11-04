6660f79a2136a2458453d4d411a635e60361b773
Kresna Sucandra <kresnasucandra@gmail.com>
2022-11-04 09:23:02 +0800

    variables and printing 2

--- msg.py
+++ msg.py
@@ -19,6 +19,7 @@ print(type(None))
 
 print(type("something"))
 
+
 # Single line comment
 
 ''' 
@@ -30,3 +31,24 @@ Multi line comment 2
 """
 
 print("inline comment") #  this is in line comment 
+
+print("take argument", "and", "print it on the screen", "\n this new line symbol makes it's printed in the new line", end=" and ended here, ")
+print("viola!")
+
+# variable
+
+x = 3
+y = "hello"
+num = x
+
+print(x, y, num)
+
+## naming variable -> 
+# meaningful
+# start with a letter (should be lowercase, because uppercase would be used in Class) or underscore. 
+# number can be used after it but any other characters is invalid
+# underscore to separate more than one (snake case is preferred, camel case is valid)
+# case sensitive (start with )
+
+
+
