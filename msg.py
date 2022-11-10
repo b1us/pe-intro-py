dc4ca68d80afbfba527f1fedc6d8e53f017a22d1
Kresna Sucandra <kresnasucandra@gmail.com>
2022-11-10 09:55:48 +0800

    variables and printing exercise

--- msg.py
+++ msg.py
@@ -51,4 +51,30 @@ print(x, y, num)
 # case sensitive (start with )
 
 
+x = 5
+y = "AlgoExpert"
+z = True
+x = y
+y = z
+z = x
+print(z)
 
+hEllo = 1
+name_input_ = 2
+# 1input = 3    invalid syntax
+# h*llo = 4    invalid syntax
+_x = 5
+# name input = 6    invalid
+# y&s = 7    invalid syntax
+# void* = 8    invalid syntax
+tim1 = 9
+# hello.World = 10    invalid syntax
+
+string = "AlgoExpert is the best!"
+print(string)
+
+x = 2
+y = 7
+z = True
+
+print(x, y, z)
