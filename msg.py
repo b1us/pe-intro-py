e1c636a087baadd9b76bec60361ad2344a6c590b
Kresna Sucandra <kresnasucandra@gmail.com>
2022-11-11 10:57:48 +0800

    console input

--- msg.py
+++ msg.py
@@ -78,3 +78,19 @@ y = 7
 z = True
 
 print(x, y, z)
+
+# console input
+input("Number: ")
+
+user_name = input("What is your username? ")
+print("Hello", user_name + "!")
+
+# console input only accepts one argument, use `string concatenation`` if you need to include multiple arguments
+# console output will always be str
+
+input("Press enter to begin!")
+
+name = input("Please anter your name: ")
+age = input("Hello, " + name + " what is your age: ")
+print("Hello,", name, "you are", age, "years old!")
+print("Hello, " + name + " you are " + age + " years old!")
