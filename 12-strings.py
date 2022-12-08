6b5ef8023e28fea2563c9d7d80aedd3808e56933
Kresna Sucandra <kresnasucandra@gmail.com>
2022-12-08 10:16:18 +0800

    string wip

---
+++ 12-strings.py
@@ -0,0 +1,25 @@
+## Strings
+
+
+s = "hEllo"
+print(s[0])
+print(s[-1])
+print(len(s))
+
+count = s.count("l")
+print(count)
+
+find = s.find("l")
+print(find)
+
+upper = s.upper()
+print(upper)
+
+lower = s.lower()
+print(lower)
+
+capital = s.capitalize()
+print(capital)
+
+contains = "h" in s
+print(contains)
