7614ffbb7437daf84da70e00f0959c596646e8f3
Kresna Sucandra <kresnasucandra@gmail.com>
2022-12-22 07:43:39 +0800

    tuples: methods

--- 13-tuples.py
+++ 13-tuples.py
@@ -3,4 +3,10 @@
 # can not directly modified (imutable)
 # keyword: tuple()
 tup = (1, True, "string")
+count = tup.count(1)
+index = tup.index("string")
+contains = 1 in tup
 print(tup)
+print(count)
+print(index)
+print(contains)
