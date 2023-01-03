2c715f0fa8a8d4b37f91e8f84d482afa01ae9401
Kresna Sucandra <kresnasucandra@gmail.com>
2023-01-03 15:43:54 +0800

    while loop: input validation

--- 15-while-loops.py
+++ 15-while-loops.py
@@ -13,3 +13,25 @@ y = 0
 while y < 10 and y % 2 == 0:
     y += 1
     print("y is", y)
+
+# Input Validation
+# Use a while loop to validate user input.
+# The input() function returns a string, so we need to convert it to an int.
+# The int() function will throw an error if the string cannot be converted to an int.
+# We can use a try/except block to catch the error and ask the user to try again.
+# The while loop will continue to ask the user for input until a valid int is entered.
+
+num = input("Enter a integer: ")
+
+while not num.isdigit():
+    print("That's not a integer!")
+    num = input("Enter a integer: ")
+
+# Another example
+z = 0
+while True:
+    try:
+        z = int(input("Enter a number: "))
+        break
+    except ValueError:
+        print("That's not a number!")
