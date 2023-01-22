abdda16cb48852f01bdd0f43d1008ad438ea2ca5
Kresna Sucandra <kresnasucandra@gmail.com>
2023-01-22 12:57:07 +0800

    sets: definition

---
+++ 18-sets.py
@@ -0,0 +1,9 @@
+# Sets
+# Definition of a set is a collection of unique elements in no particular order 
+# Sets are mutable, but the elements must be immutable
+# Sets are unordered, so you cannot access items using indexes
+# Syntax: set_name = {item1, item2, item3, ...}
+
+# Create a set
+set1 = {1, 2, 3, 4, 5}
+print(set1)
