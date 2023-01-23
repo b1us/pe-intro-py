36b3e73e05b6429f698e93b0c53350c55b2d1d01
Kresna Sucandra <kresnasucandra@gmail.com>
2023-01-23 20:46:33 +0800

    sets: add and remove

--- 18-sets.py
+++ 18-sets.py
@@ -7,3 +7,32 @@
 # Create a set
 set1 = {1, 2, 3, 4, 5}
 print(set1)
+
+# Create an empty set
+x = set() # it becomes x = {}, don't be confused with dictionary
+y = {} # it becomes empty dictionary y = {}
+z = {1, 2, 3} # it becomes set z = {1, 2, 3} as soon as we add some value in it
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Add an item / property to a set
+# Syntax: set_name.add(item)
+# If the item already exists, it will not be added
+# If the item does not exist, it will be added
+a = {1, 2, 2, 2, 3, 1, 4, 4}
+print(a) # it will print {1, 2, 3, 4}
+a.add(5)
+print(a) # it will print {1, 2, 3, 4, 5}
+a.add(5)
+print(a) # it will print {1, 2, 3, 4, 5} as 5 already exists
+
+# Remove an item / property from a set
+# Syntax: set_name.remove(item)
+# If the item does not exist, it will raise an error
+# If the item exists, it will be removed
+b = {1, 2, 3, 4, 5}
+print(b) # it will print {1, 2, 3, 4, 5}
+b.remove(5)
+print(b) # it will print {1, 2, 3, 4}
+b.remove(5) # it will raise an error as 5 does not exist
