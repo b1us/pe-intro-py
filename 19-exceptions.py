6f1f7d7aa5495c02c99815fc015dcc28f8ec695e
Kresna Sucandra <kresnasucandra@gmail.com>
2023-02-07 07:49:31 +0800

    exceptions

---
+++ 19-exceptions.py
@@ -0,0 +1,20 @@
+# Exceptions 
+# 
+# Used to handle errors in a program.
+# 
+# How to handle exceptions:
+# try:
+#     # code that might throw an exception
+# except:
+#     # code to handle the exception
+# else:
+#     # code to run if no exception was thrown
+# finally:
+#     # code to run no matter what
+#
+
+# Example 1
+try:
+    print(1 / 0)
+except:
+    print("An error occurred.")
