cfdc1d8c527c27510471813e324e4fd4d9556165
Kresna Sucandra <kresnasucandra@gmail.com>
2023-02-16 08:10:01 +0800

    functions: basix syntax

---
+++ 20-functions.py
@@ -0,0 +1,27 @@
+# Functions
+#
+# A reuseable block of code that performs a specific task and can be called upon to do something at a later time multiple times.
+# Some functions take some arguments and some don't.
+# Some functions return a value and some don't.
+# Some functions are built into Python and some are not.
+# Example of a built-in function:
+# print(), input(), len(), type(), int(), float(), str(), etc.
+# Here we're focus on creating our own functions.
+# 
+# Syntax:
+# def function_name():
+#     # code
+#
+# Example1 (no parameters to accept arguments):
+def say_hello1():
+    print("Hello!")
+
+# Example2 (takes a parameter to accept an argument):
+def say_hello2(name): # "name" is the parameter
+    print(f"Hello {name}!")
+
+# To call a function:
+say_hello1()
+say_hello2("John") # "John" is the argument
+say_hello1()
+say_hello2("Jane") # "Jane" is the argument
