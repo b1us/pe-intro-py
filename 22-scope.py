96cf2d5b47dd2211eb940e704d3e96ae1340c9fb
Kresna Sucandra <kresnasucandra@gmail.com>
2023-03-06 10:12:15 +0800

    scope: block scope example

--- 22-scope.py
+++ 22-scope.py
@@ -14,4 +14,25 @@ f()
 print(x) # 1
 # print(y) # NameError: name 'y' is not defined # local scope is not accessible from global scope => block scope => scope is defined by indentation (not by {}) => can look outside the scope but not inside
 
+# Block scope
+inp = int(input('Enter a number: '))
 
+if inp > 5:
+    value = "Greater than 5" # if the value in not greater than 5 (for example 3), the variable is not defined => NameError: name 'value' is not defined
+else:
+    value = "Less than 5"
+    # if the value already defined, it will be accessible outside the block
+
+print(value) 
+
+# block scope example from inside the function
+def f():
+    if inp2 > 5:
+        value = "Greater than 5: inside the function"
+    else:
+        value = "Less than 5: inside the function"
+    print(value) # the variable is defined inside the block => it is accessible inside the function
+
+inp2 = int(input('Enter a number: '))
+f()
+print(f"from outside the function: {value}")
