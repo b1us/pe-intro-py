94ea89f9079a7caa8e40a568d4ef90f1b088e0a8
Kresna Sucandra <kresnasucandra@gmail.com>
2023-03-17 08:03:32 +0800

    sorting: tuple

--- 24-sorting.py
+++ 24-sorting.py
@@ -18,3 +18,12 @@ print(lst) # [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1] (original list is changed)
 
 print(sorted(lst2, reverse=True)) # [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1]
 print(lst2) # [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9] (original list is unchanged)
+
+# sort a tuple
+tup = (1, 3, 2, 6, 8, 5, 9, 1, 4, 7, 1)
+print(sorted(tup)) # [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9] (returns a list) so we need to convert it back to a tuple
+print(tuple(sorted(tup))) # (1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9) (returns a tuple)
+print(tup) # (1, 3, 2, 6, 8, 5, 9, 1, 4, 7, 1) (original tuple is unchanged)
+
+# we can not do sort() on a tuple
+# tup.sort() # AttributeError: 'tuple' object has no attribute 'sort'
