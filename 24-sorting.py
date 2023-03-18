0f2533962dc98f80fdd2251e57f51f9e81d454ce
Kresna Sucandra <kresnasucandra@gmail.com>
2023-03-18 13:39:50 +0800

    sorting: by key

--- 24-sorting.py
+++ 24-sorting.py
@@ -27,3 +27,32 @@ print(tup) # (1, 3, 2, 6, 8, 5, 9, 1, 4, 7, 1) (original tuple is unchanged)
 
 # we can not do sort() on a tuple
 # tup.sort() # AttributeError: 'tuple' object has no attribute 'sort'
+
+# Sort based on a key
+# sort() and sorted() take a key argument that specifies a function to be called on each list element prior to making comparisons.
+# The key specified here should be a function that takes a single argument and returns a key to use for sorting purposes.
+# This technique is fast because the key function is called exactly once for each input record.
+# The key function can also be used to extract a portion of the element being sorted, which can be used to sort complex objects.
+# The key function can also be used to perform custom comparisons, such as case-insensitive string comparison.
+# The key function can also be used to sort objects of different types, such as sorting a list of strings and integers together.
+# The key function can also be used to sort objects of different types, such as sorting a list of strings and integers together.
+# Example: sort a list of strings by their length
+lst3 = ['a', 'bb', 'ccc', 'dddd', 'eeeee', 'ffffff', 'ggggggg']
+print(sorted(lst3, key=len)) # ['a', 'bb', 'ccc', 'dddd', 'eeeee', 'ffffff', 'ggggggg']
+print(lst3) # ['a', 'bb', 'ccc', 'dddd', 'eeeee', 'ffffff', 'ggggggg'] (original list is unchanged)
+
+# Example: sort a list of strings by their length in descending order
+print(sorted(lst3, key=len, reverse=True)) # ['ggggggg', 'ffffff', 'eeeee', 'dddd', 'ccc', 'bb', 'a']
+print(lst3) # ['a', 'bb', 'ccc', 'dddd', 'eeeee', 'ffffff', 'ggggggg'] (original list is unchanged)
+
+lst4 = [[3, 2], [1, 2], [9, 10], [5, 6], [-1, 22], [0, 0]]
+# sort by the first element of each sublist
+print(sorted(lst4)) # [[-1, 22], [0, 0], [1, 2], [3, 2], [5, 6], [9, 10]] (returns a new sorted list)
+lst4.sort()
+print(lst4) # [[-1, 22], [0, 0], [1, 2], [3, 2], [5, 6], [9, 10]] (original list is changed)
+
+# sort by the second element of each sublist
+def second_element(item):
+    return item[1]
+print(sorted(lst4, key=second_element)) # [[0, 0], [1, 2], [3, 2], [5, 6], [9, 10], [-1, 22]] (returns a new sorted list)
+
