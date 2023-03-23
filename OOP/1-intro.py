c0cb3b61c09ba59927e9ee6c16e887f64d8b4da9
Kresna Sucandra <kresnasucandra@gmail.com>
2023-03-23 19:21:57 +0800

    OOP: intro class

---
+++ OOP/1-intro.py
@@ -0,0 +1,29 @@
+# Object-oriented programming
+# Not specific to Python, but Python is an OOP language
+# Interaction objects within a program
+
+x = 1
+
+# x is an object of type int
+
+y = "some text"
+
+# y is an object of type str
+
+z = [1, 2, 3]
+
+# z is an object of type list
+
+# x, y, z are variables that store the reference to the object of specific type
+
+# we create instances of classes
+
+# class is a blueprint for creating objects
+
+print(type(x)) # <class 'int'>
+print(type(y)) # <class 'str'>
+print(type(z)) # <class 'list'>
+
+# Class is something that built in Python that defines the behavior of an object (interactions with other objects in the program)
+# Exaple:
+# x + y # TypeError: unsupported operand type(s) for +: 'int' and 'str'
