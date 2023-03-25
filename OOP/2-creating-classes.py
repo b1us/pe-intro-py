9b2433b07d48be3858c0b968b8f7921f61a0ed4a
Kresna Sucandra <kresnasucandra@gmail.com>
2023-03-26 06:09:46 +0800

    OOP: creating a class

---
+++ OOP/2-creating-classes.py
@@ -0,0 +1,10 @@
+# Creating Classes
+
+# Classes are used to create new user-defined data structures that contain arbitrary information about something
+
+# Classes are created by keyword `class`
+# Class names should be written in `PascalCase` not `camelCase`
+
+# Example:
+class PersonInTheMoon:
+    pass
