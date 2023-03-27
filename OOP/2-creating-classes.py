8eafe27e22e7065ac1cb379c71319f60962c8f2d
Kresna Sucandra <kresnasucandra@gmail.com>
2023-03-27 11:12:59 +0800

    OOP: instantiation

--- OOP/2-creating-classes.py
+++ OOP/2-creating-classes.py
@@ -8,3 +8,14 @@
 # Example:
 class PersonInTheMoon:
     pass
+
+# Instantiating a class
+# Creating an instance of a class is called instantiation
+# We can instantiate a class by calling it like a function
+# Example:
+person = PersonInTheMoon() # This is an instance of the class PersonInTheMoon
+print(person) # <__main__.PersonInTheMoon object at 0x7f9b9c0b7a90> 
+# This is the memory address of the object 
+# The __main__ part is the name of the module in which the class was defined (in this case, the file which we are running)
+print(type(person)) # <class '__main__.PersonInTheMoon'>
+# This is the type of the object
