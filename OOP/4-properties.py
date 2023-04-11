4eabdd59665cfa26ab4496e02c909cd78b6f3ecb
Kresna Sucandra <kresnasucandra@gmail.com>
2023-04-11 09:58:41 +0800

    Properties: using it in the class

--- OOP/4-properties.py
+++ OOP/4-properties.py
@@ -16,13 +16,25 @@ class Person: # most of the time the naming is singular
 
     def get_salary(self):
         return round(self._salary)
+    
+    salary = property(get_salary, set_salary)
 
 p = Person("John")
+# without property
+
 # p._salary = -100
 # print(p._salary) # -100 # can not be enforced to not change it
 # p.set_salary(-100)
 # print(p._salary) # 0
 # p.set_salary(-100)
 # print(p._salary) # ValueError: The salary can not be negative
+
+
 p.set_salary(100)
 print(p._salary) # 100
+
+# with property
+p.salary = 100
+print(p.salary) # 100
+# p.salary = -100
+# print(p.salary) # ValueError: The salary can not be negative
