663d80560e83d9fc7e255765543c888dec490c73
Kresna Sucandra <kresnasucandra@gmail.com>
2023-04-26 07:41:25 +0800

    polymorphism

--- OOP/7-inheritance.py
+++ OOP/7-inheritance.py
@@ -1,3 +1,8 @@
 # Inheritance
 
 # Inheritance is a way to form new classes using classes that have already been defined. The newly formed classes are called derived classes, the classes that we derive from are called base classes. Important benefits of inheritance are code reuse and reduction of complexity of a program. The derived classes (descendants) override or extend the functionality of base classes (ancestors).
+
+# Polymorphism
+
+# Polymorphism is the concept by which we can define methods in the child class with the same name as defined in their parent class. This helps us in defining a method in the child class that can perform a specific action depending on the child class instance.
+
