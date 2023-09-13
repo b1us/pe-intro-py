354d60a496e2bbe69f63a9cbca1d4515458b780e
Kresna Sucandra <kresnasucandra@gmail.com>
2023-09-13 10:25:49 +0800

    add abstract class def

--- OOP/8-abstract-clases.py
+++ OOP/8-abstract-clases.py
@@ -1,3 +1,16 @@
 # Abstract Class
+# A class that never has any instances. 
+# It should not be instantiated. 
+# It's meant to be a parent class, superclass, or base class for other classes.
+# It's objective is solely to increase the abstraction of the code.
+# To contain the common implementation detalis (attributes and methods) of the classes that inherit from it.
+# Methods: other than static, instance, and class methods, abstract class can have `abstract methods.
 
 # Abstract Method
+# An abstract method is a method that is declared in an abstract class but contains no implementation yet, but it is required to be implemented in the derived classes.
+
+# Notes:
+# Theoretically, Python does not have a keyword/notion to declare an abstract class.
+# We can't actualy make something abstract. We can't force it to never be able to be instantiated.
+
+# Abstract Class Example
