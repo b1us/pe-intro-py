80a2730e75dc47ca79eda1b3c38d71223fa47db9
Kresna Sucandra <kresnasucandra@gmail.com>
2023-09-27 14:05:14 +0800

    interface definition

--- OOP/9-interface.py
+++ OOP/9-interface.py
@@ -1,1 +1,10 @@
 # Interface
+
+# An interface is a contract that a class can implement to conform to a standard way of providing certain kinds of functionality.
+# It is a 100% abstract class that contains only abstract methods and static methods.
+# (It's really like abstract class but only contain abstract methods)
+# Contains no implementation details, no code other than definition of methods and also raise `NotImplementedError` inside every those methods.
+# The point of `Interface` is only define and describe all the methods that must be implemented by any class that inherits from it.
+# It's really important in `static type` languages like Java, but not that important in Python.
+# In Python we can achieve the same thing but not trully an interface.
+
