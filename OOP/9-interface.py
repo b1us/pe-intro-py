918cb278c497435c3e71d60d0e88f5c307047beb
Kresna Sucandra <kresnasucandra@gmail.com>
2023-09-28 09:59:55 +0800

    interface example

--- OOP/9-interface.py
+++ OOP/9-interface.py
@@ -8,3 +8,13 @@
 # It's really important in `static type` languages like Java, but not that important in Python.
 # In Python we can achieve the same thing but not trully an interface.
 
+# Interface examples
+
+# Example
+class RunCodeInterface: # do not needed to create `Interface` class name, just for education purpose only
+    def compile_code(self):
+        raise NotImplementedError("You must provide an implementation for compile_code() method") 
+        # do not to put any message, but it is good practice to explain why it is raised
+
+    def execuete_code(self):
+        raise NotImplementedError("You must provide an implementation for execuete_code() method")
