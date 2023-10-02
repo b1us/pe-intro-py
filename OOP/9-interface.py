c4d1e25f1bbbda3e3aa987749af79442d7264525
Kresna Sucandra <kresnasucandra@gmail.com>
2023-10-02 13:55:02 +0800

    add examples

--- OOP/9-interface.py
+++ OOP/9-interface.py
@@ -18,3 +18,21 @@ class RunCodeInterface: # do not needed to create `Interface` class name, just f
 
     def execuete_code(self):
         raise NotImplementedError("You must provide an implementation for execuete_code() method")
+
+# In other language, inherit class from an Interface would look like this:
+# class GoCode implements RunCodeInterface:
+# but in Phyton, since no real `interface` we would go with something like this:
+
+class GoCode(RunCodeInterface):
+    def compile_code(self):
+        print("Compile Go Code")
+    
+    def execute_code(self):
+        print("Execute Go Code")
+
+class JavaCode(RunCodeInterface):
+    def compile_code(self):
+        print("Compile Java Code")
+    
+    def execute_code(self):
+        print("Execute Java Code")
